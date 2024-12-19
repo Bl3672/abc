fde8a688ce14c748512d386ae0c86e5ae8335226
Bl3672 <leleyue2305@outlook.com>
2024-12-19 16:56:09 +0800

    Update static_plot.py

--- static_plot.py
+++ static_plot.py
@@ -15,11 +15,3 @@ st.line_chart(source, x="date", y="price", color="symbol",
 year = 2005
 selected_stocks=["AAPL", 'AMZN', 'IBM', 'MSFT']
 
-# the querying step won't be tested in the final
-stocks_2005 = source.query(f"date < {year + 1} and date >= {year} and symbol in {selected_stocks}")
-
-
-st.markdown(f"### Stock prices in {year}<br>", unsafe_allow_html=True)
-
-st.line_chart(stocks_2005, x="date", y="price", color="symbol",
-              width=720, height=500)
